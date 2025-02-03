8d392888e0d7e3f4900c66538dcde05c221e6bad
gyur1kim <qri1114@gmail.com>
2025-02-03 17:30:11 +0900

    chore: prettier, eslint 설정
    - prettier, eslint 관련 라이브러리 설치
    - eslint 파일 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,55 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import tsParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends(
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ),
+  {
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsParser,
+      react: reactPlugin,
+      prettier: prettierPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.ts', '.tsx'] }],
+      'import/extensions': ['error', 'ignorePackages'],
+      'no-console': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    ignores: ['build', 'dist', 'public', 'node_modules'],
+  },
 ];
-
-export default eslintConfig;
