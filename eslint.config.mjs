585886efaf34e729d83ef5c38c1762ccf98d250b
gyur1kim <qri1114@gmail.com>
2025-02-04 18:01:29 +0900

    chore: eslint.config.mjs 수정
    - 사용하지 않는 변수 warn
    - prettier 적용법 수정
    - devDependencies와 dependencies 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import { FlatCompat } from "@eslint/eslintrc";
-import eslintConfigPrettier from "eslint-config-prettier";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -12,7 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  eslintConfigPrettier,
+  ...compat.config({
+    extends: ["next", "prettier"],
+  }),
+  {
+    rules: {
+      "no-unused-vars": "warn", // 기본 JS 규칙 활성화
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
